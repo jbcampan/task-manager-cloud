c4d5c5ea04e6c278caace9d5bee973156134f0f4
Jean-Baptiste Campan <jeanbaptistecampan@gmail.com>
2026-07-23 15:27:36 +0200

    ci: update Trivy GitHub Action to v0.36.0

--- apps/backend/src/auth/strategies/jwt.strategy.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -28,6 +28,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException();
     }
-    return user; // Attached  to request.user
+    return user; // Attached to request.user
   }
 }
